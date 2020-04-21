e36a03f73a11708c449a86dafda0ab5cb3c099bf
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2020-04-21 10:26:32 +0200

    adding flatlist component

--- App.js
+++ App.js
@@ -5,108 +5,47 @@
  * @format
  * @flow strict-local
  */
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, ScrollView, View, FlatList, Text, RefreshControl } from 'react-native';
+import { POSTS } from './posts'; 
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+function Item({ title, vote }) {
+  return (
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
+      <Text style={styles.title}>{vote}</Text>
+    </View>
+  );
+}
 
 const App: () => React$Node = () => {
+  const [refreshing, setRefreshing] = useState(false);  
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <FlatList
+      data={POSTS}
+      renderItem={({ item }) => <Item title={item.title} />}
+      keyExtractor={item => item.id}
+      // onRefresh={() => setRefreshing(true)}
+      // refreshing={refreshing}
+      refreshControl={<RefreshControl refreshing={true} />}
+      inverted
+    />
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 1,
+    marginTop: 10,
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
   },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  title: {
+    fontSize: 32,
   },
 });
 
---
+++ posts.js
@@ -0,0 +1,10 @@
+const POSTS = [
+  { title: 'The weather today', votes: 10, id: '1' },
+  { title: 'The weather today', votes: 10, id: '2' },
+  { title: 'The weather today', votes: 10, id: '3' },
+  { title: 'The weather today', votes: 10, id: '4' },
+  { title: 'The weather today', votes: 10, id: '5' },
+  { title: 'The weather today', votes: 10, id: '6' },
+];
+
+export { POSTS };
