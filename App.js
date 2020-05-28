fd8146a5b61d162aacd25352714ff19ebd794010
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2020-05-28 11:06:08 +0200

    App.js minimal reproducible example

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
-import * as React from 'react';
-import { View, StyleSheet, TextInput } from 'react-native';
+import React, { useState } from "react";
+import { Button, View, StyleSheet, TextInput } from 'react-native';
 
 export default function App() {
+  const [placeholder, setPlaceholder] = useState("default");
   return (
     <View>
       <TextInput
-        placeholder="My placeholder" 
+        placeholder={placeholder}
+        style={styles.input}
       />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  input: {
+    marginTop: 50,
+    backgroundColor: "yellow",
+    textAlign: "center",
+    width: "100%",
+  },
+});
