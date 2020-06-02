43d81d8112f21b3f4fd930948b52f1aafe30e181
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2020-06-02 16:22:31 +0200

    border issue

--- App.js
+++ App.js
@@ -1,33 +1,29 @@
-import React, { useState } from 'react';
-import {Text, View, FlatList} from 'react-native';
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
 
 export default class App extends React.Component {
-  myFunc(info) {
-    console.warn('it should log me!');
-    console.warn(info);
-  }
-
-  extractor(item, index) {
-    console.log("item", item);
-    console.log("index", index);
-    return index;
-  } 
-
   render() {
-    //it not work
-    const data = [0, 5, 6, 7, 1, 1, 2, 3, 1];
-    // Using array without leading 0 will work
-    // const data = [1, 5, 6, 7, 1, 1, 2, 3, 1];
-    console.log(data);
     return (
-      <View>
-        <FlatList
-          data={data}
-          renderItem={({item}) => <Text style={{fontSize: 80}}> ss </Text>}
-          keyExtractor={(item, index) => index.toString()}
-          onViewableItemsChanged={this.myFunc}
-        />
-      </View>
+      <Text textTransform="uppercase">isdf</Text>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 100,
+    marginLeft: 100,
+    height: 100,
+    width: 200,
+    backgroundColor: 'yellow',
+    borderLeftWidth: 2,
+    borderStyle: 'dotted',
+    borderTopLeftRadius: 1,
+    borderTopEndRadius: 1,
+    borderTopRightRadius: 1,
+    borderTopRightRadius: 1,
+    borderBottomRightRadius: 1,
+    borderBottomEndRadius: 1,
+    borderBottomLeftRadius: 1,
+  },
+});
