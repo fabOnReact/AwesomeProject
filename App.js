48609d10b9b284366d762c29c3b6b1f004f187a9
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2020-06-03 16:46:52 +0200

    onKeyPress 19507

--- App.js
+++ App.js
@@ -1,29 +1,78 @@
-import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, TextInput } from 'react-native';
 
-export default class App extends React.Component {
+const BACKSPACE = 'Backspace';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      label: '',
+    };
+  }
+
+  handleKeyPress = ({ nativeEvent: { key: keyValue } }) => {
+    if (keyValue === BACKSPACE) return this.handleKeyPressed('++');
+    return this.handleKeyPressed(keyValue);
+  };
+
+  handleKeyPressed = key => {
+    this.setState({ label: `${this.state.label}${key}` });
+  };
+
   render() {
     return (
-      <Text textTransform="uppercase">isdf</Text>
+      <View style={styles.container}>
+        <Text style={styles.paragraph}>
+          Output of the onKeyPress event:
+        </Text>
+        <Text style={styles.paragraph}>
+          {this.state.label}
+        </Text>
+
+        <Text style={styles.paragraph}>
+          Numeric (dot, comma, backspace,... will work):{' '}
+        </Text>
+        <TextInput
+          ref={input => {
+            this.input = input;
+          }}
+          style={styles.input}
+          onKeyPress={this.handleKeyPress}
+          autoFocus
+          keyboardType="numeric"
+        />
+
+        <Text style={styles.paragraph}>Default: </Text>
+        <TextInput
+          ref={input => {
+            this.input = input;
+          }}
+          style={styles.input}
+          onKeyPress={this.handleKeyPress}
+          autoFocus
+        />
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 100,
-    marginLeft: 100,
-    height: 100,
-    width: 200,
-    backgroundColor: 'yellow',
-    borderLeftWidth: 2,
-    borderStyle: 'dotted',
-    borderTopLeftRadius: 1,
-    borderTopEndRadius: 1,
-    borderTopRightRadius: 1,
-    borderTopRightRadius: 1,
-    borderBottomRightRadius: 1,
-    borderBottomEndRadius: 1,
-    borderBottomLeftRadius: 1,
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    backgroundColor: '#ecf0f1',
+  },
+  input: {
+    width: 300,
+    height: 56,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#34495e',
   },
 });
