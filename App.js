c50ebe90d6e08b1254ae4291227d125cd92c9c62
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2020-06-11 09:29:47 +0200

    permissions example

--- App.js
+++ App.js
@@ -1,140 +1,93 @@
-/**
- * A test component to reproduce the crash reported at
- * https://github.com/facebook/react-native/issues/17530
- */
+import * as React from 'react';
+import { TextInput, View, TouchableOpacity, StatusBar, Text, StyleSheet } from 'react-native';
+import {PermissionsAndroid} from 'react-native';
 
-import * as React from "react";
-import { View, ScrollView, TextInput, Text, StyleSheet } from "react-native";
+async function requestAllPermissions() {
+  try {
+    const granted = await PermissionsAndroid.requestMultiple(
+    [PermissionsAndroid.PERMISSIONS.CAMERA, PermissionsAndroid.PERMISSIONS.RECORD_AUDIO]
+  );
 
-// These are the test parameters
-const params = {
-  underlineColors: ["red", undefined, "transparent", "rgba(0, 0, 0, 0)"],  // The colors to test
-  numberOfInputs: 100, // How many TextInput to render at a time
-  focusIntervalMs: 200, // How often to change focus between them
-  displayForMs: 3000, // How long to display them (set to 0 for indefinite)
-  delayDisplayForMs: 10, // How long to delay between displays
-  withScrollView: false // Whether to use a ScrollView
-};
+  if (granted[PermissionsAndroid.PERMISSIONS.CAMERA] == PermissionsAndroid.RESULTS.GRANTED) {
+    console.warn('You can use the camera');
+  } else {
+    console.warn('Camera permission denied');
+  }
 
-const testText = index =>
-  `Testing underlineColor = ${params.underlineColors[index] || "undefined"}`;
+  } catch (err) {
+    console.warn(err);
+  }
+}
 
-class AndroidTextInputTest extends React.Component{
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    requestAllPermissions();
+  }
+
   state = {
-    underlineColorIndex: 0,
-    showInputs: true,
-    startKey: 0
-  };
-  mounted = false;
-  focusInterval = undefined;
-  textInputRefs = undefined;
-  focussedInputIndex = 0;
+    statusBarVisible: true,
+  }
 
+  onPress = () => {
+    const { statusBarVisible } = this.state;
+    StatusBar.setHidden(!statusBarVisible);
+    this.setState({ statusBarVisible: !statusBarVisible })
+  }
   componentDidMount() {
-    console.log(`Testing with params = `, JSON.stringify(params));
-    this.mounted = true;
-    setTimeout(this._showInputs, params.delayDisplayForMs);
-    setInterval(this._focusAnInput, params.focusIntervalMs);
   }
-
-  componentWillUnmount() {
-    clearInterval(this.focusInterval);
-    this.mounted = false;
-  }
-
-  _focusAnInput = () => {
-    if (this.mounted && this.textInputRefs) {
-      if (this.focussedInputIndex >= this.textInputRefs.length) {
-        this.focussedInputIndex = 0;
-      }
-      const textInputRef = this.textInputRefs[this.focussedInputIndex];
-      const textInput = this.refs[textInputRef];
-      if (textInput) {
-        this.focussedInputIndex++;
-        this.refs[textInputRef].focus();
-      }
-    }
-  };
-
-  _showInputs = () => {
-    if (this.mounted) {
-      console.log(testText(this.state.underlineColorIndex));
-      this.setState({ showInputs: true });
-      if (params.displayForMs) {
-        setTimeout(this._unshowInputs, params.displayForMs);
-      }
-    }
-  };
-
-  _unshowInputs = () => {
-    this.focussedInputIndex = 0;
-    this.textInputRefs = undefined;
-    if (this.mounted) {
-      let next = this.state.underlineColorIndex + 1;
-      if (next === params.underlineColors.length) {
-        next = 0;
-      }
-      this.setState({
-        underlineColorIndex: next,
-        showInputs: false,
-        startKey: this.state.startKey + params.numberOfInputs
-      });
-      setTimeout(this._showInputs, params.delayDisplayForMs);
-    }
-  };
 
   render() {
-    const textInputs = [];
-    const { underlineColorIndex } = this.state;
-    const underlineColor = params.underlineColors[underlineColorIndex];
-
-    const refs = [];
-
-    if (this.state.showInputs) {
-      for (let i = 0; i < params.numberOfInputs; i++) {
-        const key = this.state.startKey + i + "";
-        refs.push(key);
-        textInputs.push(
-          <TextInput
-            ref={key}
-            key={key}
-            placeholder={key}
-            underlineColorAndroid={underlineColor}
-            style={styles.textInput}
-          />
-        );
-      }
-      if (!this.textInputRefs) {
-        this.textInputRefs = refs;
-      }
-    }
-
     return (
-      <View style={styles.mainView}>
-        <Text>{testText(underlineColorIndex)}</Text>
-        {params.withScrollView ? (
-          <React.Fragment>
-            <Text>With ScrollView</Text>
-            <ScrollView>{textInputs}</ScrollView>
-          </React.Fragment>
-        ) : (
-          <React.Fragment>{textInputs}</React.Fragment>
-        )}
+      <View style={styles.container}>
+        <TextInput style={styles.textInput} placeholder=' Press to open kayboard'/>
+        <TouchableOpacity style={styles.button} onPress={this.onPress}>
+          <Text>Press to change statusbar visibility</Text>
+        </TouchableOpacity>
+        <View
+          style={{marginTop:300}}>
+          <Text>My Test</Text>
+          <Text>My Test</Text>
+          <Text>My Test</Text>
+          <Text>My Test</Text>
+          <Text>My Test</Text>
+          <Text>My Test</Text>
+          <Text>My Test</Text>
+          <Text>My Test</Text>
+          <Text>My Test</Text>
+          <Text>My Test</Text>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  mainView: {
+  container: {
     flex: 1,
-    alignItems: "center"
+    borderWidth: 5,
+    borderColor: 'red'
   },
+  
   textInput: {
-    backgroundColor: "white",
-    margin: 5,
-    width: 300
+    width: '80%',
+    height: 26,
+    marginTop: 50,
+    alignSelf: 'center',
+    borderColor: 'blue',
+    borderWidth: 1,
+  },
+  
+  button: {
+    width: '80%',
+    height: 50,
+    marginTop: 50,
+    alignSelf: 'center',
+    borderColor: 'blue',
+    borderWidth: 2,
+    borderRadius: 10,
+    
+    alignItems: 'center',
+    justifyContent: 'center',
   }
 });
-
-export default AndroidTextInputTest;
