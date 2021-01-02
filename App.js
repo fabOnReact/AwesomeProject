0fb4297b61fb0b19ff8d83522e809f3f47eaf553
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2021-01-02 16:51:08 +0100

    MRE https://github.com/facebook/react-native/issues/30158

--- App.js
+++ App.js
@@ -1,27 +1,62 @@
-import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, View, SafeAreaView, PermissionsAndroid, Button } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.paragraph}>
-        Change code in the editor and watch it change on your phone! Save to get a shareable url.
-      </Text>
-    </View>
-  );
-}
+const checkCameraPermission = async () => {
+  try {
+    const status = await PermissionsAndroid.check(
+      PermissionsAndroid.PERMISSIONS.CAMERA,
+    );
+    console.warn('status', status)
+  } catch (err) {
+    console.warn(err);
+  }
+};
+
+const requestCameraPermission = async () => {
+  try {
+    const status = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.CAMERA,
+      {
+        title: "Cool Photo App Camera Permission",
+        message:
+          "Cool Photo App needs access to your camera ",
+        buttonNeutral: "Ask Me Later",
+        buttonNegative: "Cancel",
+        buttonPositive: "OK"
+      }
+    );
+    console.warn('status', status)
+  } catch (err) {
+    console.warn(err);
+  }
+};
+
+class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.item}>Try permissions</Text>
+        <Button title="check camera" onPress={checkCameraPermission} />
+        <Button title="request camera" onPress={requestCameraPermission} />
+      </View>
+    );
+  }
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#ecf0f1',
-    padding: 8,
+    justifyContent: "center",
+    paddingTop: 10,
+    backgroundColor: "#ecf0f1",
+    padding: 8
   },
-  paragraph: {
+  item: {
     margin: 24,
     fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
+    fontWeight: "bold",
+    textAlign: "center"
+  }
 });
+
+export default App;
